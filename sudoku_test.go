75dfcf94020965e7e271089fe92f206d011469ac
urzq <clement.lenhof@gmail.com>
2018-10-14 23:01:00 +0200

    more unit test

--- sudoku_test.go
+++ sudoku_test.go
@@ -4,54 +4,93 @@ import (
 	"testing"
 )
 
-func TestCreateGrid_2_2(t *testing.T) {
-	src := `
-		2 3 | 1 2 
-		5 6 | 4 5 
-		---------
-		1 2 | . 2 
-		. 8 | 7 8`
+var grid_str_2_2 = `
+	2 3 | 1 2 
+	5 6 | 4 5 
+	---------
+	1 2 | . 2 
+	. 8 | 7 8`
 
-	grid := createGridFromString(src)
+var grid_str_2_3 = `
+	1 2 | 2 3 | 2 3
+	4 5 | 5 6 | 5 6
+	7 8 | 8 9 | . 9
+	---------------
+	1 2 | 1 2 | 2 3
+	4 5 | 4 5 | 5 6 
+	7 8 | 7 8 | 7 9`
 
-	if grid.Size != 4 {
-		t.Errorf("Incorrect grid size. (%d instead of 4)", grid.Size)
+var grid_str_3_2 = `
+	1 2 3 | 1 2 3
+	4 5 6 | 4 5 6
+	-------------
+	1 2 3 | 1 2 3
+	7 8 9 | 7 8 9
+	-------------
+	1 2 3 | 1 2 3 
+	7 8 9 | 7 8 9`
+
+var grid_str_3_3 = `
+	1 2 3 | 1 2 3 | 1 2 3
+	4 5 6 | 4 5 6 | . 5 6
+	7 8 9 | 7 8 9 | 7 . 9
+	---------------------
+	1 2 3 | 1 2 3 | 1 2 3
+	4 5 6 | 4 5 6 | 4 5 6
+	7 8 9 | 7 8 9 | 7 8 9
+	---------------------
+	1 2 3 | 1 2 3 | 1 2 3
+	4 5 6 | 4 5 6 | 4 5 6
+	7 8 9 | 7 8 9 | 7 8 9`
+
+var grid_str_4_4 = `
+	 1  2  3 10 |  1  2  3 10 |  1  2  3 10 |  1  2  3 10
+	 4  5  6 11 |  4  5  6 11 |  4  5  6 11 |  4  5  6 11
+	 7  8  9 12 |  7  8  9 12 |  7  8  9 12 |  7  8  9 12
+	13 14 15 16 | 13 14 15 16 | 13 14 15 16 | 13 14 15 16
+	-----------------------------------------------------
+	 1  2  3 10 |  1  2  3 10 |  1  2  3 10 |  1  2  3 10
+	 4  5  6 11 |  4  5  6 11 |  4  5  6 11 |  4  5  6 11
+	 7  8  9 12 |  7  8  9 12 |  7  8  9 12 |  7  8  9 12
+	13 14 15 16 | 13 14 15 16 | 13 14 15 16 | 13 14 15 16
+	-----------------------------------------------------
+	 1  2  3 10 |  1  2  3 10 |  1  2  3 10 |  1  2  3 10
+	 4  5  6 11 |  4  5  6 11 |  4  5  6 11 |  4  5  6 11
+	 7  8  9 12 |  7  8  9 12 |  7  8  9 12 |  7  8  9 12
+	13 14 15 16 | 13 14 15 16 | 13 14 15 16 | 13 14 15 16
+	-----------------------------------------------------
+	 1  2  3 10 |  1  2  3 10 |  1  2  3 10 |  1  2  3 10
+	 4  5  6 11 |  4  5  6 11 |  4  5  6 11 |  4  5  6 11
+	 7  8  9 12 |  7  8  9 12 |  7  8  9 12 |  7  8  9 12
+	13 14 15 16 | 13 14 15 16 | 13 14 15 16 | 13 14 15 16`
+
+func TestCreateGrid(t *testing.T) {
+	tests := []struct {
+		gridStr              string
+		expectedSize         int
+		expectedRegionWidth  int
+		expectedRegionHeight int
+	}{
+		{gridStr: grid_str_2_2, expectedSize: 4, expectedRegionWidth: 2, expectedRegionHeight: 2},
+		{gridStr: grid_str_2_3, expectedSize: 6, expectedRegionWidth: 2, expectedRegionHeight: 3},
+		{gridStr: grid_str_3_2, expectedSize: 6, expectedRegionWidth: 3, expectedRegionHeight: 2},
+		{gridStr: grid_str_3_3, expectedSize: 9, expectedRegionWidth: 3, expectedRegionHeight: 3},
+		{gridStr: grid_str_4_4, expectedSize: 16, expectedRegionWidth: 4, expectedRegionHeight: 4},
 	}
 
-	if grid.RegionWidth != 2 {
-		t.Errorf("Incorrect RegionWidth (%d instead of 2)", grid.RegionWidth)
-	}
+	for _, test := range tests {
+		grid := createGridFromString(test.gridStr)
 
-	if grid.RegionHeight != 2 {
-		t.Errorf("Incorrect RegionHeight (%d instead of 2)", grid.RegionHeight)
-	}
-}
+		if grid.Size != test.expectedSize {
+			t.Errorf("Incorrect grid size. (%d instead of %d)", grid.Size, test.expectedSize)
+		}
 
-func TestCreateGrid_3_3(t *testing.T) {
-	var src = `
-		1 2 3 | 1 2 3 | 1 2 3
-		4 5 6 | 4 5 6 | . 5 6
-		7 8 9 | 7 8 9 | 7 . 9
-		---------------------
-		1 2 3 | 1 2 3 | 1 2 3
-		4 5 6 | 4 5 6 | 4 5 6
-		7 8 9 | 7 8 9 | 7 8 9
-		---------------------
-		1 2 3 | 1 2 3 | 1 2 3
-		4 5 6 | 4 5 6 | 4 5 6
-		7 8 9 | 7 8 9 | 7 8 9`
+		if grid.RegionWidth != test.expectedRegionWidth {
+			t.Errorf("Incorrect RegionWidth (%d instead of %d)", grid.RegionWidth, test.expectedRegionWidth)
+		}
 
-	grid := createGridFromString(src)
-
-	if grid.Size != 9 {
-		t.Errorf("Incorrect grid size. (%d instead of 9)", grid.Size)
-	}
-
-	if grid.RegionWidth != 3 {
-		t.Errorf("Incorrect RegionWidth (%d instead of 3)", grid.RegionWidth)
-	}
-
-	if grid.RegionHeight != 3 {
-		t.Errorf("Incorrect RegionHeight (%d instead of 2)", grid.RegionHeight)
+		if grid.RegionHeight != test.expectedRegionHeight {
+			t.Errorf("Incorrect RegionHeight (%d instead of %d)", grid.RegionHeight, test.expectedRegionHeight)
+		}
 	}
 }
